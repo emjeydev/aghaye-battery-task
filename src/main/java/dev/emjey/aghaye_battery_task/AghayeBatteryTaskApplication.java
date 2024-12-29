74fb42dc3d8bfd8128bc9a2b7bab8b864168500c
EmJeyDev <emjeydev@gmail.com>
2024-12-29 15:25:04 +0330

    added the project

---
+++ src/main/java/dev/emjey/aghaye_battery_task/AghayeBatteryTaskApplication.java
@@ -0,0 +1,24 @@
+package dev.emjey.aghaye_battery_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This file is made by EmJey
+ * Project: AghayeBatteryTestTask
+ * FileName: AghayeBatteryTaskApplication.java
+ * Date: 2024/12/29
+ * Modified Date: 2024/12/29
+ * Email: emjeydev@gmail.com
+ * GitHub: emjeydev
+ */
+
+
+@SpringBootApplication
+public class AghayeBatteryTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AghayeBatteryTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/emjey/aghaye_battery_task/AghayeBatteryTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emjey.aghaye_battery_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AghayeBatteryTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
